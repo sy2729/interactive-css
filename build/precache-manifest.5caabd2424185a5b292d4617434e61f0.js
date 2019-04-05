e2ce62cf9cf04ef3be998d051ba34029bc0cec97
sy2729 <sy2729@tc.columbia.edu>
2019-04-05 19:34:20 -0400

    unignore build folder

---
+++ build/precache-manifest.5caabd2424185a5b292d4617434e61f0.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "3837837ddfd213066a33",
+    "url": "/static/css/0.eb716357.chunk.css"
+  },
+  {
+    "revision": "3837837ddfd213066a33",
+    "url": "/static/js/0.6dac426b.chunk.js"
+  },
+  {
+    "revision": "e5508c69621726f1942f",
+    "url": "/static/css/1.835d477a.chunk.css"
+  },
+  {
+    "revision": "e5508c69621726f1942f",
+    "url": "/static/js/1.7532216f.chunk.js"
+  },
+  {
+    "revision": "66248c03e1f4cc3a1079",
+    "url": "/static/js/2.d119959f.chunk.js"
+  },
+  {
+    "revision": "71efde0dcb0199615130",
+    "url": "/static/js/3.8845b2d0.chunk.js"
+  },
+  {
+    "revision": "7fe6165c27e83d84e0e9",
+    "url": "/static/js/4.311424c4.chunk.js"
+  },
+  {
+    "revision": "305ddbeac48ddfdbc5c1",
+    "url": "/static/js/5.bbea1729.chunk.js"
+  },
+  {
+    "revision": "2a330a616b59af52c64c",
+    "url": "/static/js/6.0ff4c740.chunk.js"
+  },
+  {
+    "revision": "14e1d90ff3805f905a3e",
+    "url": "/static/js/7.196ed09a.chunk.js"
+  },
+  {
+    "revision": "9ebaa74aed7423fca1fb",
+    "url": "/static/js/8.05644572.chunk.js"
+  },
+  {
+    "revision": "4662c4ee915c6bcfa895",
+    "url": "/static/js/9.c4462127.chunk.js"
+  },
+  {
+    "revision": "bb664c770a64f65a4c81",
+    "url": "/static/js/10.5ec2d571.chunk.js"
+  },
+  {
+    "revision": "f2d24ddd060892016f32",
+    "url": "/static/js/11.11219518.chunk.js"
+  },
+  {
+    "revision": "a73a4301eeab7d4d3476",
+    "url": "/static/js/12.2253b3de.chunk.js"
+  },
+  {
+    "revision": "cc6ffbe68b05e3f52446",
+    "url": "/static/js/13.a8956d38.chunk.js"
+  },
+  {
+    "revision": "805ebe88975ee68edc8d",
+    "url": "/static/css/main.1b5256c2.chunk.css"
+  },
+  {
+    "revision": "805ebe88975ee68edc8d",
+    "url": "/static/js/main.cec83504.chunk.js"
+  },
+  {
+    "revision": "66e07efe0f9db420f650",
+    "url": "/static/js/runtime~main.4659ae54.js"
+  },
+  {
+    "revision": "d3870033ed5d1d918097",
+    "url": "/static/css/16.551f59bb.chunk.css"
+  },
+  {
+    "revision": "d3870033ed5d1d918097",
+    "url": "/static/js/16.73b63f9d.chunk.js"
+  },
+  {
+    "revision": "781fa9c6a3cdb2839fdb0a53d843cfdc",
+    "url": "/index.html"
+  }
+];
